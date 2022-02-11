27475ab54074001a6acf999f708b14e712e52bb7
Igor Galvao Bezerra <igorgalvaob@gmail.com>
2022-02-10 21:40:00 -0300

    #

--- app.js
+++ app.js
@@ -108,15 +108,11 @@ app.get('/callback', function(req, res) {
         });
         
         // we can also pass the token to the browser to make requests from there
-        /*   res.redirect('https://spotry.herokuapp.com/#' +
+           res.redirect('https://spotry.herokuapp.com/#' +
         querystring.stringify({
           access_token: access_token,
           refresh_token: refresh_token
-        })); */
-        res.json({
-          access_token: access_token,
-          refresh_token: refresh_token
-        })
+        }))
       } else {
         res.redirect('/#' +
         querystring.stringify({
