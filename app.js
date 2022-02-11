4123b03a97b16a86f27db5fd3000e2ff39d41d79
Igor Galvao Bezerra <igorgalvaob@gmail.com>
2022-02-11 00:31:33 -0300

    #

--- app.js
+++ app.js
@@ -136,9 +136,9 @@ app.get('/refresh_token', function(req, res) {
   request.post(authOptions, function(error, response, body) {
    // if (!error && response.statusCode === 200) {
      // var access_token = body.access_token;
-      res.send({
-        'access_token': refresh_token,
-        b:'teste'
+      res.send(
+        //'access_token': refresh_token,
+        JSON.stringify(req.body)
         //access_token
      // });
     //}
