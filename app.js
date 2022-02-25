3385f81fa4dd4cbeb81dc0dec960f9423bb92e4d
Igor Galvao Bezerra <igorgalvaob@gmail.com>
2022-02-24 22:46:13 -0300

    #

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.get('/login', function(req, res) {
   res.cookie(stateKey, state);
 
   // your application requests authorization
-  var scope = 'user-read-private user-read-email user-read-playback-state';
+  var scope = 'playlist-read-private playlist-modify-public playlist-read-collaborative playlist-modify-private user-read-recently-played user-top-read user-read-playback-position app-remote-control streaming streaming user-library-modify user-follow-read user-follow-modify user-follow-modify user-read-private user-read-currently-playing user-modify-playback-state user-read-playback-state ugc-image-upload';
   res.redirect('https://accounts.spotify.com/authorize?' +
     querystring.stringify({
       response_type: 'code',
